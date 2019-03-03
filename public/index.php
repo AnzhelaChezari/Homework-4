fa90a3f067a87d8bf001e32e6ae403f288a19eea
AnzhelaChezari <anzhmalakh@gmail.com>
2019-03-03 18:28:18 +0300

    добавила один пункт к шаблонизатору

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ require_once __DIR__ . '/../config/config.php';
 echo render(TEMPLATES_DIR . 'index.tpl', [
     'H1'=> 'Холодильное оборудование',
     'TITLE' => 'галерея с холодильниками',
+    'H2' => 'Задание сделано через scandir , но при вызове функции добавляются несуществующие фотки',
+    'CONTENT' => BuildImg(),
+
 ]);
 
 
