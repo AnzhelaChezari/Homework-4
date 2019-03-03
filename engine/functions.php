e7ffd1f61981dd87b5c23aa5269903098fb08a14
AnzhelaChezari <anzhmalakh@gmail.com>
2019-03-03 18:27:40 +0300

    добавила новую функцию

--- engine/functions.php
+++ engine/functions.php
@@ -1,11 +1,13 @@
 <?php
+require_once __DIR__ . '/../config/config.php';
+
 
 function render ($file, $variables=[]) {
     if (!is_file($file)) {
         echo 'Templates "' . $file . '" doesnt exist';
         exit();    
     }
-    if (filesize($file)===0) {
+    if (filesize($file) === 0) {
        echo 'Templates "' . $file . '" is empty';
         exit(); 
     }
@@ -21,12 +23,25 @@ foreach ($variables as $key => $value) {
 		if (empty($value) || !is_string($value)) {
 			continue;
 		}
-      $key = '{{' . strtoupper($key) . '}}';
-    $templateContent = str_replace($key, $value, $templateContent);
+      $key = '{{' . strtoupper( $key) . '}}';
+    $templateContent = str_replace( $key, $value, $templateContent);
   }
 return $templateContent;
   }
     
+function BuildImg() {
+    $result = '';
+    $dir = "img/img-2";
+    $images = array_diff( scandir($dir), array('..', '.'));
+    foreach ( $images as $image) {
+      if (is_file(WWW_DIR . 'img/img-2/' . $image)) {  
+          $result .= render(TEMPLATES_DIR . 'gallery.tpl', [
+             'SRC' => IMG_DIR . $image,                      
+          ]);   
+      }
+    } 
+    return $result;         
+}
 
 ?>
 
